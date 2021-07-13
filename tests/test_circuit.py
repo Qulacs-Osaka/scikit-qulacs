4b8c38ac86a49baca9c142d95580a7656aa1e683
ikanago <ikanago@ikanago.dev>
2021-07-13 15:33:46 +0900

    Fix test

--- tests/test_circuit.py
+++ tests/test_circuit.py
@@ -5,12 +5,10 @@ def test_parametric_gate():
     circuit = LearningCircuit(2, 0.0)
     circuit.add_input_RX_gate(1)
     circuit.add_RX_gate(0, 0.5)
-    circuit.add_parametric_RX_gate(0, 0.3)
-    print([circuit._circuit.get_parameter(i) for i in range(2)])
+    circuit.add_parametric_RX_gate(0, 0.0)
     circuit.add_parametric_RX_gate(1, 0.0)
     circuit.update_parameters([0.1, 0.2])
     assert [0.1, 0.2] == circuit.get_parameters()
-    assert False
 
 
 def test_parametric_input_gate():
