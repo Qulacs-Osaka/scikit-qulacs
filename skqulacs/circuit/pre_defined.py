153a7988e1df6a0937d28088a8440a44b8e08bc3
WATLE <31515524+WATLE@users.noreply.github.com>
2021-07-27 17:21:53 +0900

    Update pre_defined.py

--- skqulacs/circuit/pre_defined.py
+++ skqulacs/circuit/pre_defined.py
@@ -39,3 +39,33 @@ def create_ansatz(n_qubit: int, c_depth: int, time_step: float) -> LearningCircu
             angle = 2.0 * np.pi * np.random.rand()
             circuit.add_parametric_RX_gate(i, angle)
     return circuit
+
+def create_farhi_circuit(n_qubit: int, c_depth: int, time_step: float) -> LearningCircuit:
+    def preprocess_x(x: List[float], index: int):
+        xa = x[index % len(x)]
+        return min(1, max(-1, xa))
+
+    circuit = LearningCircuit(n_qubit)
+    for i in range(n_qubit):
+        circuit.add_input_RY_gate(i, lambda x,i=i: np.arcsin(preprocess_x(x, i)))
+        circuit.add_input_RZ_gate(
+            i, lambda x,i=i: np.arccos(preprocess_x(x, i) * preprocess_x(x, i))
+        )
+
+    zyu = list(range(n_qubit))
+    for _ in range(c_depth):
+        random.shuffle(zyu)
+        #今回の回路はdepthを多めにとったほうがいいかも
+        #最低でもn_qubitはほしいかも
+        for i in range(0,n_qubit-1,2):
+            anglex = 2.0 * np.pi * np.random.rand() 
+            angley = 2.0 * np.pi * np.random.rand() 
+            circuit.add_CNOT_gate(zyu[i+1],zyu[i])
+            circuit.add_parametric_RX_gate(zyu[i], anglex)
+            circuit.add_parametric_RY_gate(zyu[i], angley)
+            circuit.add_CNOT_gate(zyu[i+1],zyu[i])
+            anglex = 2.0 * np.pi * np.random.rand() 
+            angley = 2.0 * np.pi * np.random.rand() 
+            circuit.add_parametric_RY_gate(zyu[i], -angley)
+            circuit.add_parametric_RX_gate(zyu[i], -anglex)
+    return circuit
