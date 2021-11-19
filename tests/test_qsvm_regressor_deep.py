a912d707f775191030a132ba231a5a13941b81ab
WATLE <31515524+WATLE@users.noreply.github.com>
2021-11-19 20:14:29 +0900

    81 deep qsv (#101)
    * deep qsv 追加
    
    * qsvm消して、testの回数少なくして、　Adamの学習率調整した
    
    * setup変えた

--- tests/test_qsvm_regressor_deep.py
+++ tests/test_qsvm_regressor_deep.py
@@ -3,7 +3,7 @@ import random
 from numpy.random import RandomState
 from skqulacs.qsvm import QSVR
 from sklearn.metrics import mean_squared_error
-from skqulacs.circuit import create_defqsv
+from skqulacs.circuit import create_deepqsv
 
 
 def func_to_learn(x):
@@ -38,12 +38,12 @@ def test_noisy_sine():
     x_train, y_train = generate_noisy_sine(x_min, x_max, num_x)
     x_test, y_test = generate_noisy_sine(x_min, x_max, num_test)
     n_qubit = 6
-    circuit = create_defqsv(n_qubit, 4)
+    circuit = create_deepqsv(n_qubit, 4)
     qsvm = QSVR(circuit)
     qsvm.fit(x_train, y_train)
     y_pred = qsvm.predict(x_test)
     loss = mean_squared_error(y_pred, y_test)
-    assert loss < 0.008
+    assert loss < 0.005
 
 
 # 2要素のSVMを試してみる
