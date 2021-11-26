0675aa26975a692a4f5c74e436dafaf4b30d1419
Wataru Inoue <wattan1729@gmail.com>
2021-11-26 20:17:18 +0900

    narabinaosita

--- skqulacs/circuit/__init__.py
+++ skqulacs/circuit/__init__.py
@@ -1,8 +1,8 @@
 from .circuit import LearningCircuit
 from .graph import show_blochsphere
 from .pre_defined import (
-    create_defqsv,
     create_deepqsv,
+    create_defqsv,
     create_farhi_circuit,
     create_farhi_watle,
     create_qcl_ansatz,
