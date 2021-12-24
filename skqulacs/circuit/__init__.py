eeddc3ba4dd7835cd1e9b686fffaf9de1d239f47
Wataru Inoue <wattan1729@gmail.com>
2021-12-24 16:45:07 +0900

    画像認識のipynbした

--- skqulacs/circuit/__init__.py
+++ skqulacs/circuit/__init__.py
@@ -4,6 +4,7 @@ from .pre_defined import (
     create_farhi_neven_watle_ansatz,
     create_ibm_embedding_circuit,
     create_largeqsv,
+    create_largeqsv_YZCX,
     create_qcl_ansatz,
     create_shirai_ansatz,
 )
