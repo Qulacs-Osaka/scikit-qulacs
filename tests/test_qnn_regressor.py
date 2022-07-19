95af2381007073c8954addaac055a17d767930f9
ikanago <ikanago-dev@protonmail.com>
2022-07-19 07:48:49 +0000

    Refactor generate_*() in test_qnn_regressor.py

--- tests/test_qnn_regressor.py
+++ tests/test_qnn_regressor.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import Callable, Optional, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,21 +12,33 @@ from skqulacs.qnn import QNNRegressor
 from skqulacs.qnn.solver import Adam, Bfgs, Solver
 
 
-def two_vars_two_outputs(x: NDArray[np.float_]) -> NDArray[np.float_]:
-    return np.array([2.0 * x[0] + x[1], 1.5 * x[0] - 3.0 * x[1]])
-
-
-def generate_noisy_two_vars_two_outputs(
-    x_min: float, x_max: float, num_x: int
+def generate_noisy_data(
+    x_min: float,
+    x_max: float,
+    x_shape: Tuple[int, int],
+    function: Callable[[NDArray[np.float_]], NDArray[np.float_]],
+    seed: Optional[int] = 0,
+    mag_noise: float = 0.001,
 ) -> Tuple[NDArray[np.float_], NDArray[np.float_]]:
-    rng = default_rng(0)
-    x_train = np.array(
-        [[rng.uniform(x_min, x_max), rng.uniform(x_min, x_max)] for _ in range(num_x)]
-    )
-    y_train = np.array([two_vars_two_outputs(x) for x in x_train])
-    mag_noise = 0.001
+    """Generate random input data and its output
+
+    Args:
+        x_min: Minimum limit of random x value.
+        x_max: Maximum limit of random x value.
+        x_shape: Shape of x, (batch, features).
+        function: Function which generates output from x. It takes 1D feature vector and returns 1D output vector.
+        seed: Seed for random value.
+        mag_noise: Noise amplitude to be added to output.
+    """
+    rng = default_rng(seed)
+    x_train = rng.uniform(x_min, x_max, x_shape)
+    y_train = np.array([function(x) for x in x_train])
     y_train += mag_noise * rng.random(y_train.shape)
     return x_train, y_train
+
+
+def two_vars_two_outputs(x: NDArray[np.float_]) -> NDArray[np.float_]:
+    return np.array([2.0 * x[0] + x[1], 1.5 * x[0] - 3.0 * x[1]])
 
 
 @pytest.mark.parametrize(("solver", "maxiter"), [(Bfgs(), 30), (Adam(), 30)])
@@ -34,7 +46,9 @@ def test_noisy_two_vars_two_outputs(solver: Solver, maxiter: int) -> None:
     x_min = -0.5
     x_max = 0.5
     num_x = 50
-    x_train, y_train = generate_noisy_two_vars_two_outputs(x_min, x_max, num_x)
+    x_train, y_train = generate_noisy_data(
+        x_min, x_max, (num_x, 2), two_vars_two_outputs
+    )
 
     n_qubit = 4
     depth = 3
@@ -43,28 +57,15 @@ def test_noisy_two_vars_two_outputs(solver: Solver, maxiter: int) -> None:
     qnn = QNNRegressor(circuit, solver)
     qnn.fit(x_train, y_train, maxiter)
 
-    x_test, y_test = generate_noisy_two_vars_two_outputs(x_min, x_max, num_x)
+    x_test, y_test = generate_noisy_data(x_min, x_max, (num_x, 2), two_vars_two_outputs)
     y_pred = qnn.predict(x_test)
     loss = mean_squared_error(y_pred, y_test)
     assert loss < 0.11
     return x_test, y_test, y_pred
 
 
-def sine_two_vars(x: List[float]) -> float:
+def sine_two_vars(x: NDArray[np.float_]) -> NDArray[np.float_]:
     return np.sin(np.pi * x[0] * x[1])
-
-
-def generate_noisy_sine_two_vars(
-    x_min: float, x_max: float, num_x: int
-) -> Tuple[NDArray[np.float_], NDArray[np.float_]]:
-    rng = default_rng(0)
-    x_train = np.array(
-        [[rng.uniform(x_min, x_max), rng.uniform(x_min, x_max)] for _ in range(num_x)]
-    )
-    y_train = np.array([sine_two_vars(x) for x in x_train])
-    mag_noise = 0.001
-    y_train += mag_noise * rng.random(num_x)
-    return x_train, y_train
 
 
 @pytest.mark.parametrize(("solver", "maxiter"), [(Bfgs(), 20), (Adam(), 20)])
@@ -72,7 +73,7 @@ def test_noisy_sine_two_vars(solver: Solver, maxiter: int) -> None:
     x_min = -0.5
     x_max = 0.5
     num_x = 50
-    x_train, y_train = generate_noisy_sine_two_vars(x_min, x_max, num_x)
+    x_train, y_train = generate_noisy_data(x_min, x_max, (num_x, 2), sine_two_vars)
 
     n_qubit = 4
     depth = 3
@@ -81,26 +82,15 @@ def test_noisy_sine_two_vars(solver: Solver, maxiter: int) -> None:
     qnn = QNNRegressor(circuit, solver)
     qnn.fit(x_train, y_train, maxiter)
 
-    x_test, y_test = generate_noisy_sine_two_vars(x_min, x_max, num_x)
+    x_test, y_test = generate_noisy_data(x_min, x_max, (num_x, 2), sine_two_vars)
     y_pred = qnn.predict(x_test)
     loss = mean_squared_error(y_pred, y_test)
     assert loss < 0.1
     return x_test, y_test, y_pred
 
 
-def sine(x: float) -> float:
-    return np.sin(np.pi * x)
-
-
-def generate_noisy_sine(
-    x_min: float, x_max: float, num_x: int
-) -> Tuple[NDArray[np.float_], NDArray[np.float_]]:
-    rng = default_rng(0)
-    x_train = np.array([[rng.uniform(x_min, x_max)] for _ in range(num_x)])
-    y_train = np.array([sine(x[0]) for x in x_train])
-    mag_noise = 0.01
-    y_train += mag_noise * rng.random(num_x)
-    return x_train, y_train
+def sine(x: NDArray[np.float_]) -> NDArray[np.float_]:
+    return np.sin(np.pi * x[0])
 
 
 @pytest.mark.parametrize(
@@ -111,7 +101,7 @@ def test_noisy_sine(solver: Solver, maxiter: int) -> None:
     x_min = -1.0
     x_max = 1.0
     num_x = 50
-    x_train, y_train = generate_noisy_sine(x_min, x_max, num_x)
+    x_train, y_train = generate_noisy_data(x_min, x_max, (num_x, 1), sine)
 
     n_qubit = 3
     depth = 3
@@ -120,7 +110,7 @@ def test_noisy_sine(solver: Solver, maxiter: int) -> None:
     qnn = QNNRegressor(circuit, solver)
     qnn.fit(x_train, y_train, maxiter)
 
-    x_test, y_test = generate_noisy_sine(x_min, x_max, num_x)
+    x_test, y_test = generate_noisy_data(x_min, x_max, (num_x, 1), sine)
     y_pred = qnn.predict(x_test)
     loss = mean_squared_error(y_pred, y_test)
     assert loss < 0.03
