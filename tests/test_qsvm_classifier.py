117b59642e349e06c6ea8758ac3c8663a43343f4
Aum Patel <73774338+patel-aum@users.noreply.github.com>
2022-12-29 19:45:08 +0530

    Optimized it

--- tests/test_qsvm_classifier.py
+++ tests/test_qsvm_classifier.py
@@ -1,29 +1,34 @@
 import pandas as pd
 from sklearn import datasets
-from sklearn.metrics import f1_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
 
 from skqulacs.circuit import create_ibm_embedding_circuit
 from skqulacs.qsvm import QSVC
 
+iris = datasets.load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+x = df.loc[:, ["petal length (cm)", "petal width (cm)"]]
+x_train, x_test, y_train, y_test = train_test_split(
+    x, iris.target, test_size=0.25, random_state=1
+)
 
-def test_classify_iris():
-    iris = datasets.load_iris()
-    df = pd.DataFrame(iris.data, columns=iris.feature_names)
-    x = df.loc[:, ["petal length (cm)", "petal width (cm)"]]
-    x_train, x_test, y_train, y_test = train_test_split(
-        x, iris.target, test_size=0.25, random_state=1
-    )
+x_train = x_train.to_numpy()
+x_test = x_test.to_numpy()
+n_qubit = 4  # qubitの数 2だと少なすぎて複雑さがでない
+circuit = create_ibm_embedding_circuit(n_qubit)
 
-    x_train = x_train.to_numpy()
-    x_test = x_test.to_numpy()
-    n_qubit = 4  # qubitの数 2だと少なすぎて複雑さがでない
-    circuit = create_ibm_embedding_circuit(n_qubit)
-    qsvm = QSVC(circuit)
-    qsvm.fit(x_train, y_train)
-    y_pred = qsvm.predict(x_test)
-    assert f1_score(y_test, y_pred, average="weighted") > 0.92
+param_grid = {
+    "C": [0.1, 1, 10, 100],
+    "kernel": ["linear", "poly", "rbf"],
+    "gamma": ["scale", "auto"],
+}
 
+qsvm = QSVC(circuit)
+grid_search = GridSearchCV(qsvm, param_grid, cv=5, n_jobs=-1)
+grid_search.fit(x_train, y_train)
 
-if __name__ == "__main__":
-    test_classify_iris()
+print(f"Best parameters: {grid_search.best_params_}")
+print(f"Best score: {grid_search.best_score_}")
+
+y_pred = grid_search.predict(x_test)
+print(f"Test score: {grid_search.score(x_test, y_test)}")
